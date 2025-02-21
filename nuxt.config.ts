314def79fbdbbaa906f8c358e09af795d7a70cb5
Nikki D'Ambra <42721110+ndambra@users.noreply.github.com>
2025-02-21 12:01:30 -0500

    Add files via upload

---
+++ nuxt.config.ts
@@ -0,0 +1,34 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/color-mode", "@nuxt/content"],
+  content: {
+    highlight: {
+      theme: {
+        default: "min-light",
+        dark: "min-dark",
+      },
+    },
+  },
+  components: {
+    global: true,
+    dirs: ["~/components"],
+  },
+  router: {
+    options: {
+      scrollBehaviorType: "smooth",
+    },
+  },
+  tailwindcss: {
+    config: {
+      plugins: [require("@tailwindcss/typography")],
+    },
+  },
+});
